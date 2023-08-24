6321d2ca95e22eba7f45afddbe9946ea3069d255
Kim Sang Bin <bloodyrosa@naver.com>
2023-08-24 10:23:25 +0900

    commit

--- src/template_ui.js
+++ src/template_ui.js
@@ -1,10 +1,16 @@
 
 import '../public/css/hsad_common.css';
 import '../public/css/components.css';
+import '../public/css/dummy.css';
 
 //---------------------------
 
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
+gsap.registerPlugin(ScrollTrigger);
 
+gsap.to('.dummy-block-2 .box-1', {
 
+});
 
